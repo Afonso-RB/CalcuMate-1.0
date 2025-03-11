47b9ba18652aec774b385704454cee3d18b334ef
Rúben Bumba <rubenbumba56@gmail.com>
2025-03-11 15:06:00 +0100

    Stanford NLP

---
+++ CalcuMate 1.0/NplProcessor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using edu.stanford.nlp.ling;
+using edu.stanford.nlp.pipeline;
+using edu.stanford.nlp.util;
+using java.util;
+using Console = System.Console;
+
+namespace CalcuMate_1._0
+{
+    public class NlpProcessor
+    {
+        private readonly StanfordCoreNLP _pipeline;
+
+        public NlpProcessor()
+        {
+            var props = new Properties();
+            props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse");
+            _pipeline = new StanfordCoreNLP(props);
+        }
+
+        public string ProcessInput(string input)
+        {
+            var annotation = new Annotation(input);
+            _pipeline.annotate(annotation);
+
+            var sentences = annotation.get(typeof(CoreAnnotations.SentencesAnnotation)) as ArrayList;
+            if (sentences == null || sentences.size() == 0)
+                return input;
+
+            var sentence = sentences.get(0) as CoreMap;
+            var tokens = sentence.get(typeof(CoreAnnotations.TokensAnnotation)) as ArrayList;
+
+            var processedInput = string.Join(" ", tokens.Cast<CoreLabel>().Select(token => token.lemma()));
+            return processedInput;
+        }
+    }
+}
--- CalcuMate 1.0/WolframAlphaClient.cs
+++ CalcuMate 1.0/WolframAlphaClient.cs
@@ -15,6 +15,7 @@ namespace CalcuMate_1._0
         private static readonly IConfiguration Configuration;
         private static readonly string AppId;
         private static readonly string BaseUrl = "http://api.wolframalpha.com/v2/query";
+        private static readonly NlpProcessor NlpProcessor;
 
         static WolframAlphaClient()
         {
@@ -23,13 +24,16 @@ namespace CalcuMate_1._0
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
             Configuration = builder.Build();
             AppId = Configuration["WolframAlpha:ApiKey"] ?? throw new InvalidOperationException("A chave da API do Wolfram Alpha não está definida no arquivo de configuração.");
+            NlpProcessor = new NlpProcessor();
         }
 
         public async Task<string> QueryWolframAlpha(string input)
         {
+            string processedInput = NlpProcessor.ProcessInput(input);
+
             using (var client = new HttpClient())
             {
-                string url = $"{BaseUrl}?input={Uri.EscapeDataString(input)}&appid={AppId}&format=plaintext";
+                string url = $"{BaseUrl}?input={Uri.EscapeDataString(processedInput)}&appid={AppId}&format=plaintext";
                 HttpResponseMessage response = await client.GetAsync(url);
                 response.EnsureSuccessStatusCode();
                 string responseData = await response.Content.ReadAsStringAsync();
